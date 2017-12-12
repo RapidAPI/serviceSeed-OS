9bc92c058b5a7b6582e76e4c51fce234cd9e56ea
noamsalinger1991 <noamsalinger1991@gmail.com>
2017-12-12 11:57:45 +0200

    refactoring

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-//const seed = require("endpoint-seed");
+const webFramework = require("./webFramework/Interface")
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const exepetions = require("./utils/exeptions");
 
-//serviceUsageExample
-const cluster     = require('cluster');
-const seed = require("./server");
-const joi = require("joi");
-const { NODE_ENV = "" } = process.env;
+module.exports.exepetions = exepetions;
 
-const helloEndPoint = {
-    endpoint: async () => {
-        return { status: 200, data: "Hello" };
-    },
-    validations: {
-        query: { a: joi.string().required() }
+let seed = (exports = module.exports = { config: {} });
+
+seed.setConfig = ({ port, webFramework, }) => {
+    seed.config.webFramework = webFramework;
+    seed.config.port = port;
+}
+
+seed.init = () => {
+    webFramework.init(seed.config);
+}
+
+
+seed.routes = (routes) => {
+    routes.forEach(route => {
+        const { routeName, endpoint, validations = {}, method } = route;
+        webFramework.expose(method, routeName, endpoint, validations)
+    });
+}
+
+seed.listen = (callback) => {
+    try {
+        webFramework.listen(seed.config, callback);
     }
-};
-
-const routes =
-
-    [{
-        routeName: "/v2/hello",
-        endpoint: helloEndPoint.endpoint,
-        validations: helloEndPoint.validations,
-        method: "get"
+    catch (e) {
+        callback(e);
     }
-    ];
-
-seed.setConfig({ port: "8080", webFramework: "express" , enableClusters: cluster.isMaster && NODE_ENV !== "test" , enableSwagger : NODE_ENV !== "production"});
-seed.init();
-seed.routes(routes);
-seed.listen((error) => { if (error) { console.log(error) } });
-
-
+}
